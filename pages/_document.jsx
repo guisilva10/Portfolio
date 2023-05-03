9efbe292d8177fe01f616fd4dfc86747a11282f2
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2023-05-03 02:58:52 -0300

    add title to website

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,6 +4,7 @@ export default function Document (){
   return (
     <Html>
       <Head>
+        <title>Guilherme Lopes</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2:family=Roboto:wght@400;500;700&display=swap" />
